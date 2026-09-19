9bbb52e4c2c8db87ca606ba2835df72a6f93d7ee
Henry <9660755@gmail.com>
2026-09-20 00:02:35 +0800

    Add new domain entries to singbox_to_shadowrocket
    Added new entries for apple-domain, applestore, chinamax_to_ipv6, and chinamax_domain to the list.

--- scripts/singbox_to_shadowrocket.py
+++ scripts/singbox_to_shadowrocket.py
@@ -60,6 +60,10 @@ RULESETS = [
     "apple-cn",
     "apple@cn",
     "apple",
+    "apple-domain",
+    "applestore",
+    "chinamax_to_ipv6",
+    "chinamax_domain",
     "cloudflare",
     "jd",
     "alibaba",
@@ -70,6 +74,7 @@ RULESETS = [
     "douyin",
     "cn",
     "private",
+    "lan"
 ]
 
 # Primary source (jsdelivr CDN) and a raw.githubusercontent fallback.
