24588a931113c0d95c6db925e6742de6db363595
Henry <9660755@gmail.com>
2026-09-20 00:06:40 +0800

    Update singbox_to_loon.py

--- scripts/singbox_to_loon.py
+++ scripts/singbox_to_loon.py
@@ -67,10 +67,6 @@ RULESETS = [
     "apple-cn",
     "apple@cn",
     "apple",
-    "apple-domain",
-    "applestore",
-    "chinamax_to_ipv6",
-    "chinamax_domain",
     "cloudflare",
     "jd",
     "alibaba",
@@ -80,8 +76,7 @@ RULESETS = [
     "xiaohongshu",
     "douyin",
     "cn",
-    "private",
-    "lan"
+    "private"
 ]
 
 # Primary source (jsdelivr CDN) and a raw.githubusercontent fallback.
